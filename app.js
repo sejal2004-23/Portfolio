df399b78acf49e27d1a15ea6b73c0c978e966b30
sejal2004-23 <dalvi23sejal@gmail.com>
2025-04-05 19:59:06 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const hamb = document.querySelector('.header .nav-bar .nav-list .hamb');
-const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
-const header = document.querySelector('.header.container');
-
-humb.addEventListener('click',() => {
-    humb.classlist.toggle('active');
-    mobile_menu.classlist.toggle('active');
-});
